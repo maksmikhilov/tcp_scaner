579178c92fe06f788c2aadc352331f3ea91f6eaa
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-08 17:03:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import socket
+server = socket.socket() 
+server.bind(("0.0.0.0", 5000)) 
+server.listen(4) 
+client_socket, client_address = server.accept()
+print(client_address, "has connected")
+while True:
+    recvieved_data = client_socket.recv(1024)
+    print(recvieved_data)
