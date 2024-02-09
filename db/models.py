975f023e970001c8afcd56924a2bf0eaadba9122
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 12:28:34 +0300

    fix

--- db/models.py
+++ db/models.py
@@ -9,8 +9,6 @@ Base = declarative_base()
 
 class TcpInfo(Base):
     __tablename__ = 'tcp'
-
-    id = Column(Integer, primary_key=True)
     name = Column(String, default=None)
     host = Column(String, default=None)
     port = Column(Integer, default=None)
@@ -18,16 +16,13 @@ class TcpInfo(Base):
     second_query = Column(String, default=None)
     timeout = Column(Integer, default=None)
     request_interval = Column(Integer, default=None)
-    create_time = Column(DateTime, default=datetime.now)
 
 class TcpResult(Base):
     __tablename__ = 'tcp_result'
 
-    id = Column(Integer, primary_key=True)
     name = Column(String, default=None)
     status = Column(String, default=None)
     tmstmp = Column(String, default=None)
     request_time = Column(String, default=None)
     first_response = Column(String, default=None)
     second_response = Column(String, default=None)
-    create_time = Column(DateTime, default=datetime.now)
