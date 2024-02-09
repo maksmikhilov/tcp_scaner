8b78672578329782ca8901a0df4a52f055b674ff
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 13:11:53 +0300

    fix

--- db/connection.py
+++ db/connection.py
@@ -8,24 +8,26 @@ engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB
 SyncSession = sessionmaker(bind=engine)
 models.Base.metadata.create_all(engine)
 with SyncSession() as session:
-    first = {
-        "host": '176.113.83.95',
-        "port": 22,
-        "timeout": 3,
-        "request_interval": 3,
-        "first_request": 'bim',
-        "second_request": 'ueiekn',
-        "name": 'Server A'
-    }
-    second = {
-        "host": '45.141.102.225',
-        "port": 22,
-        "timeout": 3,
-        "request_interval": 3,
-        "first_request": 'poe',
-        "second_request": 'bsjs',
-        "name": 'Server b'
-    }
-    session.add(models.TcpInfo(**first))
-    session.add(models.TcpInfo(**second))
-    session.commit()
+    table = session.query(models.TcpInfo)
+    if not list(table.all()):
+        first = {
+            "host": '176.113.83.95',
+            "port": 22,
+            "timeout": 3,
+            "request_interval": 3,
+            "first_request": 'bim',
+            "second_request": 'ueiekn',
+            "name": 'Server A'
+        }
+        second = {
+            "host": '45.141.102.225',
+            "port": 22,
+            "timeout": 3,
+            "request_interval": 3,
+            "first_request": 'poe',
+            "second_request": 'bsjs',
+            "name": 'Server b'
+        }
+        session.add(models.TcpInfo(**first))
+        session.add(models.TcpInfo(**second))
+        session.commit()
