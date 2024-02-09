939a45f19d091069e453317ec0d7c988423ee26a
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 08:40:27 +0300

    fix

--- db/connection.py
+++ db/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 engine = create_engine(
-    f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
     echo=False,
 )
 Base = automap_base()
