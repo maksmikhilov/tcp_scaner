9b7459ff5995e7f49773b265a3f196adfc493a1b
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 11:07:00 +0300

    fix

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def check_tcp(params):
         responses = []
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(timeout)
+            s.settimeout(10)
             start_time = time.time()
             s.connect((host, port))
             for query in [first_query, second_query]:
                 s.send(query.encode())
-                responses.append(s.recv(1024))
+                response = s.recv(1024)
+                responses.append(response)
             end_time = time.time()
             wait_time = end_time - start_time
             s.close()
@@ -31,6 +32,7 @@ def check_tcp(params):
                 "first_response": responses[0],
                 "second_response": responses[1]
             }
+            print(tcp_data)
             interface.set_row(Tcp_result, tcp_data)
         except Exception as e:
             print(e)
@@ -38,9 +40,11 @@ def check_tcp(params):
         
 def run_task_with_timeout(params):
     p = multiprocessing.Process(target=check_tcp, args=(params,))
+    print('Запрос к ', params[0])
     p.start()
     p.join(timeout=8)
     if p.is_alive():
+        print('Убиваем ', params[0])
         p.terminate()
         p.join()
 
@@ -49,6 +53,8 @@ while True:
     tasks = []
     for TCP in TCPs:
         name, host, port = TCP.name, TCP.host, TCP.port
+        print(host)
+        print(port)
         first_query, second_query = TCP.first_query, TCP.second_query
         timeout, interval = TCP.timeout, TCP.request_interval
         params = (name, host, port, first_query, second_query, timeout, interval)
@@ -56,6 +62,7 @@ while True:
         with ProcessPoolExecutor(max_workers=2) as executor:
             executor.map(run_task_with_timeout, tasks)
         time.sleep(10)
+        print('Получаем новые данные')
         
         
         
