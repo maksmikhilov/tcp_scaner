564d0af0ea2edcd8bccee54d0f776fb034a695cb
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 09:52:29 +0300

    fix

--- db/connection.py
+++ db/connection.py
@@ -6,5 +6,7 @@ engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
     echo=False,
 )
-result = engine.execute("SELECT * FROM tcp")
-print(result)
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM tcp')
+    data = rs.fetchall()
+    print(data)
