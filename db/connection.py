2d0d8071b320f7442d73db0d21144511ca314d61
maksmikhilov <maksmikhilov01@gmail.com>
2024-02-09 09:55:28 +0300

    fix

--- db/connection.py
+++ db/connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.ext.automap import automap_base
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
@@ -6,7 +6,5 @@ engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
     echo=False,
 )
-with engine.connect() as con:
-    rs = con.execute('SELECT * FROM "tcp"')
-    data = rs.fetchall()
-    print(data)
+inspector = inspect(engine)
+print(inspector.get_columns('book'))
